8c94767962b555975a25336cb10254d7dcb00a7c
gitjon625 <120992113+gitjon625@users.noreply.github.com>
2022-12-31 12:11:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,12 @@ streamlit.dataframe(fruityvice_normalized)
 #Connecting Snowflake to Streamlit
 import snowflake.connector
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowfake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 
 
